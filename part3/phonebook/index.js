f97d099a524e8cccbe3bf149a785209753fda228
Abhay2412 <abhaykhosla0@gmail.com>
2023-05-30 20:40:56 -0600

    Fetching and creating to the database with the frontend, and also creating the module person with all the mongoose connection

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,43 +1,26 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const Person = require("./models/person")
+
+require('dotenv').config()
 
 morgan.token('body', (request) => JSON.stringify(request.body))
 app.use(express.json())
 app.use(morgan('tiny'))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get("/api/persons", (request, response) => {
-    response.json(persons)
+  Person.find({}).then(result => {
+    response.json(result)
+  })
 })
 
 app.get("/info", (request, response) => {
     const currentDate = new Date()
     console.log(currentDate)
     console.log(persons.length)
-    response.send(`<p>Phonebook has information for ${persons.length} persons</p> <p>${currentDate}</p>`)
+    response.send(`<p>Phonebook has information for ${Person.length} persons</p> <p>${currentDate}</p>`)
 })
 
 app.get("/api/persons/:id", (request, response) => {
@@ -52,48 +35,62 @@ app.get("/api/persons/:id", (request, response) => {
 })
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+  // const id = Number(request.params.id)
+  // persons = Person.filter(person => person.id !== id)
 
-  response.status(204).end()
+  // response.status(204).end()
+  Person.findById(request.params.id)
+  .then(person => {
+    if(person) {
+      response.json(person)
+    }
+    else {
+      response.status(404).end()
+    }
+  })
 })
 
-const generateId = () => {
-  const randomId = Math.floor(Math.random() * 1000)
-  return randomId
-}
+// const generateId = () => {
+//   const randomId = Math.floor(Math.random() * 1000)
+//   return randomId
+// }
 
 const postMorgan = morgan(':method :url :status :res[content-length] - :response-time ms :body')
 
 app.post("/api/persons", postMorgan, (request, response) => {
   const body = request.body
-  const personName = persons.map(persons => persons.name)
+  // const personName = persons.map(persons => persons.name)
+  // const personName = Person.map(persons => persons.name)
   
   if(!body.name || !body.number) {
     return response.status(400).json({
       error: "The name or number is missing"
     })
   }
-  else if(personName.includes(body.name)) {
-    return response.status(400).json({
-      error: "The name must be unique"
-    })
-  }
+  // else if(personName.includes(body.name)) {
+  //   return response.status(400).json({
+  //     error: "The name must be unique"
+  //   })
+  // }
   else {
-    const personToAdd = {
-      id: generateId(), 
+    const person = new Person ({
+      // id: generateId(), 
       name: body.name, 
       number: body.number,
-    }
+    })
 
-    persons = persons.concat(personToAdd)
-    response.json(personToAdd)
+    person.save().then(personToAdd => {
+      response.json(personToAdd)
+    })
+
+    // persons = persons.concat(personToAdd)
+    // response.json(personToAdd)
 
   }
 })
 
 
-const port = process.env.PORT || 3001
-app.listen(port, () =>
-console.log(`Server running on http://localhost:${port}`)
+const PORT = process.env.PORT
+app.listen(PORT, () =>
+console.log(`Server running on http://localhost:${PORT}`)
 );
