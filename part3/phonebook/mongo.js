7107dbd9bc76a5288ca9c63c58aae159798be3dd
Abhay2412 <abhaykhosla0@gmail.com>
2023-05-30 19:44:02 -0600

    Finish Exercise 3.12 and learn about mongoose and creating a database

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log("Give password as an argument")
+    process.exit(1)
+}
+
+const userPassword = process.argv[2]
+
+const url = `mongodb+srv://abhay2412:${userPassword}@cluster1.441lb.mongodb.net/FullStackOpen2023Part3?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv.length===3){
+
+    Person.find({}).then(result => {
+      console.log('Phonebook: ')
+      result.forEach(person => {
+        console.log(`${person.name} ${person.number}`)
+      })
+      mongoose.connection.close()
+    })
+}
+else {
+    const person = new Person({
+      name: process.argv[3],
+      number: process.argv[4],
+    }) 
+  
+    person.save().then(result => {
+      console.log(`Added ${result.name} number ${result.number} to the phonebook`)
+      mongoose.connection.close()
+    })
+  }
