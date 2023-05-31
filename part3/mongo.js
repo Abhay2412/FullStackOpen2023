d6632bd0e2483a5fe2b367728e1ee17cdf1b606b
Abhay2412 <abhaykhosla0@gmail.com>
2023-05-31 16:30:52 -0600

    Fix lint issues

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log("Give password as an argument")
-    process.exit(1)
+  console.log('Give password as an argument')
+  process.exit(1)
 }
 
 const userPassword = process.argv[2]
@@ -13,30 +13,29 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if(process.argv.length===3){
 
-    Person.find({}).then(result => {
-      console.log('Phonebook: ')
-      result.forEach(person => {
-        console.log(`${person.name} ${person.number}`)
-      })
-      mongoose.connection.close()
+  Person.find({}).then(result => {
+    console.log('Phonebook: ')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
     })
+    mongoose.connection.close()
+  })
 }
 else {
-    const person = new Person({
-      name: process.argv[3],
-      number: process.argv[4],
-    }) 
-  
-    person.save().then(result => {
-      console.log(`Added ${result.name} number ${result.number} to the phonebook`)
-      mongoose.connection.close()
-    })
-  }
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
+  person.save().then(result => {
+    console.log(`Added ${result.name} number ${result.number} to the phonebook`)
+    mongoose.connection.close()
+  })
+}
