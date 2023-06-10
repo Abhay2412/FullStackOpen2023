e0a83a02cfe889cc8cf223a5aec23757f65fe5ce
Abhay2412 <abhaykhosla0@gmail.com>
2023-06-09 21:40:25 -0600

    Writing tests with async/await for the Blog apis

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,20 +1,43 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+// blogsRouter.get('/', (request, response) => {
+//   Blog.find({})
+//     .then(blogs => {
+//       response.json(blogs)
+//     })
+// })
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blogToAdd = new Blog(request.body)
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if(blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
+})
 
-  blogToAdd.save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  const blogToAdd = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes ? body.likes: 0
+  })
+
+  if(body.title === undefined || body.url === undefined) {
+    response.status(400).end()
+  } else {
+    const blogToSave = await blogToAdd.save()
+    response.status(201).json(blogToSave)
+  }
 })
 
 module.exports = blogsRouter
